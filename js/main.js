cefae9c432e3527467757bd36da0203512af3765
varun singh <varun8884@gmail.com>
2022-10-14 10:37:30 +0530

    menu

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// ====== EXPENDER MENU ======
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId) 
+    
+    if(toggle && nav){
+        toggle.addEventListener('click', () =>{
+            nav.classList.toggle('show')
+            toggle.classList.toggle('bx-x')
+        })
+    }
+}
+
+showMenu('header-toggle','nav-menu')
+
+// ====== ACTIVE AND REMOVE MENU ======
+const navLink = document.getElementById('.nav__link')
+
+function linkAction(){
+    navLink.forEach(n => n.classList.remove('active')) 
+    this.classList.add('active')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
